bef176d3954e845681e8e38e022bd554b27597d4
shadrxcc <akaadesm@gmail.com>
2023-10-02 18:22:37 +0100

    sdivfhvek

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const assistant = document.getElementById("assistant");
 const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
 const button3 = document.getElementById("button3");
+const button4 = document.getElementById("button4");
+const button5 = document.getElementById("button5");
+const reset = document.getElementById("reset");
 
 const timeline = gsap.timeline();
 
-//initial position and transparency of the assistant
 gsap.set(assistant, {
   x: 0,
   y: 600,
@@ -15,7 +17,6 @@ gsap.set(assistant, {
   height: "50px",
 });
 
-//event listeners for button interactions
 button1.addEventListener("click", () => {
   gsap.to(assistant, {
     x: button1.offsetLeft,
@@ -36,13 +37,55 @@ button2.addEventListener("click", () => {
 
 button3.addEventListener("click", () => {
   gsap.to(assistant, {
+    x: button3.offsetLeft,
+    y: button3.offsetTop + 20,
+    duration: 1,
+  });
+  showMessage("You clicked Button 3.");
+});
+
+button4.addEventListener("click", () => {
+  gsap.to(assistant, {
+    x: button4.offsetLeft,
+    y: button4.offsetTop + 20,
+    duration: 1,
+  });
+  showMessage("You clicked Button 4.");
+});
+
+button5.addEventListener("click", () => {
+  gsap.to(assistant, {
+    x: button5.offsetLeft,
+    y: button5.offsetTop + 20,
+    duration: 1,
+  });
+  showMessage("You clicked Button 5.");
+});
+
+reset.addEventListener("click", () => {
+  assistant.innerHTML = ''
+  gsap.to(assistant, {
     x: 0,
     y: window.innerHeight - assistant.offsetHeight,
     duration: 1,
   });
 });
 
-// function to show a message
 function showMessage(message) {
-  console.log(message);
+  assistant.innerHTML = '';
+
+  const messageElement = document.createElement("div");
+  messageElement.textContent = message;
+
+  messageElement.style.padding = "10px";
+  messageElement.style.backgroundColor = "lightgray";
+  messageElement.style.borderRadius = "5px";
+  messageElement.style.marginTop = "10px";
+
+  assistant.appendChild(messageElement);
+
+  setTimeout(() => {
+    assistant.removeChild(messageElement);
+  }, 5000);
 }
+
